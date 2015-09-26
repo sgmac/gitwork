10bd05acc3a3e5736aa0d949df569643e9f312c9
Sergio Galvan <sergalma@gmail.com>
2015-09-25 20:02:07 -0500

    Test git repositories

--- main_test.go
+++ main_test.go
@@ -2,18 +2,95 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path"
 	"testing"
+	"time"
 
 	"github.com/Sirupsen/logrus"
+	"github.com/libgit2/git2go"
 )
 
 func init() {
+	gitPathEnv = "/tmp/git"
 	configPath = "/tmp/.gitwork"
 	configFile = "config"
 	err := createConfigFile()
 	if err != nil {
 		logrus.Fatal(err)
+	}
+}
+
+func createGitRepositories() {
+
+	signature := &git.Signature{
+		Name:  "John Doe",
+		Email: "johndoe@nowhere.com",
+		When:  time.Now(),
+	}
+
+	repositories := []struct {
+		Repo          string
+		CommitMessage string
+	}{
+		{path.Join(gitPathEnv, "test1"), "First commit"},
+		{path.Join(gitPathEnv, "test2"), "Initial commit"},
+		{path.Join(gitPathEnv, "nongit"), ""},
+	}
+	for _, r := range repositories {
+		if r.CommitMessage == "" {
+			os.Mkdir(r.Repo, 0755)
+			continue
+		}
+
+		// git init
+		gitRepo, err := git.InitRepository(r.Repo, false)
+		if err != nil {
+			logrus.Fatal("0: ", err)
+		}
+
+		// create content for the file
+		filename := path.Join(r.Repo, "file")
+		data := []byte(`This is a file`)
+		err = ioutil.WriteFile(filename, data, 0755)
+		if err != nil {
+			logrus.Fatal("1: ", err)
+		}
+
+		// get repo's index
+		index, err := gitRepo.Index()
+		if err != nil {
+			logrus.Fatal("2: ", err)
+		}
+
+		// add content to the index/staging area, file
+		// must be relative to the repository.
+		err = index.AddByPath("file")
+		if err != nil {
+			logrus.Fatal("3: ", err)
+		}
+
+		treeId, err := index.WriteTree()
+		if err != nil {
+			logrus.Fatal("4: ", err)
+		}
+
+		err = index.Write()
+		if err != nil {
+			logrus.Fatal("5: ", err)
+		}
+
+		tree, err := gitRepo.LookupTree(treeId)
+		if err != nil {
+			logrus.Fatal("6: ", err)
+		}
+
+		// Create commit, as this is the first commit, there are no parents.
+		_, err = gitRepo.CreateCommit("refs/heads/master", signature, signature, r.CommitMessage, tree)
+		if err != nil {
+			logrus.Fatal("7: ", err)
+		}
 	}
 }
 
@@ -35,4 +112,19 @@ func TestReadConfigFile(t *testing.T) {
 	defer func() {
 		os.RemoveAll(configPath)
 	}()
+}
+
+func TestGetRepositories(t *testing.T) {
+	// Create two git repositories and one non git repo. This
+	// gets all the directories
+	createGitRepositories()
+	reposExpected := []string{"/tmp/git/test1", "/tmp/git/test2", "/tmp/git/nongit"}
+	repos, err := getRepositories(gitPathEnv)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	if len(reposExpected) != len(repos) {
+		t.Error("Repositories not found")
+	}
 }
